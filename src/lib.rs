b10ed0da53b2424466fe4068dc3430ecde0b6547
Makoto <makoto@emurasoft.com>
2020-07-19 13:13:23 -0700

    Use Yew

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,10 @@ pub mod block;
 pub mod savefile;
 pub mod sprite;
 
+use log::info;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use yew::prelude::*;
 
 error_chain::error_chain! {
     types {
@@ -27,27 +30,61 @@ impl std::convert::Into<wasm_bindgen::JsValue> for Error {
     }
 }
 
-#[allow(unused_macros)]
-macro_rules! console_log {
-    ( $( $t:tt )* ) => {
-        web_sys::console::log_1(&format!( $( $t )* ).into());
-    }
-}
-
 #[wasm_bindgen(start)]
 pub fn start() -> Result<()> {
-    use wasm_bindgen::JsCast;
-    console_error_panic_hook::set_once();
-    let document = web_sys::window().unwrap().document().unwrap();
-    let canvas: web_sys::HtmlCanvasElement = document.create_element("canvas")?.dyn_into().unwrap();
-    canvas.set_attribute("width", "400");
-    canvas.set_attribute("height", "300");
-    canvas.set_attribute("style", "border: 1px solid black");
-    let ctx: web_sys::CanvasRenderingContext2d =
-        canvas.get_context("2d")?.unwrap().dyn_into().unwrap();
-    js_sys::Reflect::set(&ctx, &"font".into(), &"20px sans-serif".into());
-    ctx.fill_text("Hello world", 10.0, 50.0)?;
-
-    document.body().unwrap().append_with_node_1(&canvas)?;
+    wasm_logger::init(wasm_logger::Config::default());
+    App::<Page>::new().mount_to_body();
     Ok(())
+}
+
+struct Page {
+    link: ComponentLink<Self>,
+    canvas_ref: NodeRef,
+}
+
+enum Msg {}
+
+impl Component for Page {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
+            canvas_ref: NodeRef::default(),
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> bool {
+        false
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> bool {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <canvas
+                    ref={self.canvas_ref.clone()}
+                    width="400"
+                    height="300"
+                    style="border: 1px solid black"
+                />
+            </div>
+        }
+    }
+
+    fn rendered(&mut self, _: bool) {
+        let canvas: web_sys::HtmlCanvasElement = self.canvas_ref.cast().unwrap();
+        let ctx: web_sys::CanvasRenderingContext2d = canvas
+            .get_context("2d")
+            .unwrap()
+            .unwrap()
+            .dyn_into()
+            .unwrap();
+        js_sys::Reflect::set(&ctx, &"font".into(), &"20px sans-serif".into());
+        ctx.fill_text("Hello world", 10.0, 50.0).unwrap();
+    }
 }
