12ab0d720745f51749febae8e2c19468dc1e8265
Makoto <makoto@emurasoft.com>
2021-01-05 12:38:28 -0800

    TracedRwLock

--- src/traced_rwlock.rs
+++ src/traced_rwlock.rs
@@ -1,209 +1,135 @@
 use super::*;
-use std::ops::Index;
+use std::collections::VecDeque;
 use tokio::sync::{RwLockReadGuard, RwLockWriteGuard};
 
 #[derive(Debug)]
 pub struct TracedRwLock<T> {
     inner: RwLock<T>,
-    recent_calls: RefCell<FixedSizeBuffer>,
+    recent_access: RefCell<RecentAccess>,
 }
 
 impl<T> TracedRwLock<T> {
     pub fn new(inner: T) -> Self {
         Self {
             inner: RwLock::new(inner),
-            recent_calls: RefCell::new(FixedSizeBuffer::default()),
+            recent_access: RefCell::new(RecentAccess::Read(VecDeque::new())),
         }
     }
 
     pub async fn read(&self, file: &'static str, line: u32) -> RwLockReadGuard<'_, T> {
-        let mut trace = Trace {
-            read_or_write: ReadOrWrite::Read,
-            action: Action::Waiting,
-            file,
-            line,
-        };
-        if cfg!(debug_assertions) {
-            self.recent_calls.borrow_mut().add(trace);
-        }
-
         let result = self.inner.read().await;
         if cfg!(debug_assertions) {
-            trace.action = Action::Acquired;
-            self.recent_calls.borrow_mut().add(trace);
+            self.recent_access
+                .borrow_mut()
+                .add_read(Trace { file, line });
         }
         result
     }
 
     pub async fn write(&self, file: &'static str, line: u32) -> RwLockWriteGuard<'_, T> {
-        let mut trace = Trace {
-            read_or_write: ReadOrWrite::Write,
-            action: Action::Waiting,
-            file,
-            line,
-        };
-        if cfg!(debug_assertions) {
-            self.recent_calls.borrow_mut().add(trace);
-        }
-
         let result = self.inner.write().await;
         if cfg!(debug_assertions) {
-            trace.action = Action::Acquired;
-            self.recent_calls.borrow_mut().add(trace);
+            self.recent_access
+                .borrow_mut()
+                .add_write(Trace { file, line });
         }
         result
     }
 
     #[allow(dead_code)]
     pub fn trace(&self) -> String {
-        format!("{}", self.recent_calls.borrow())
+        format!("{}", self.recent_access.borrow())
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+enum RecentAccess {
+    Read(VecDeque<Trace>),
+    Write(Trace),
+}
+
+impl RecentAccess {
+    fn add_read(&mut self, trace: Trace) {
+        match self {
+            RecentAccess::Read(r) => {
+                r.push_back(trace);
+                if r.len() > 8 {
+                    r.pop_front();
+                }
+            }
+            RecentAccess::Write(_) => *self = RecentAccess::Read(vec![trace].into()),
+        }
+    }
+
+    fn add_write(&mut self, trace: Trace) {
+        match self {
+            RecentAccess::Read(_) => *self = RecentAccess::Write(trace),
+            RecentAccess::Write(w) => *w = trace,
+        }
+    }
+}
+
+impl Display for RecentAccess {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RecentAccess::Read(reads) => {
+                f.write_str("RwLock was last read: [\n")?;
+                for trace in reads {
+                    write!(f, "\t{},\n", trace)?;
+                }
+                f.write_str("]")
+            }
+            RecentAccess::Write(write) => {
+                f.write_str("RwLock was last written: ")?;
+                write!(f, "{}", write)
+            }
+        }
     }
 }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 struct Trace {
-    read_or_write: ReadOrWrite,
-    action: Action,
     file: &'static str,
     line: u32,
 }
 
 impl Default for Trace {
     fn default() -> Self {
-        Self {
-            read_or_write: ReadOrWrite::Read,
-            action: Action::Waiting,
-            file: "",
-            line: 0,
-        }
+        Self { file: "", line: 0 }
     }
 }
 
 impl Display for Trace {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{} {} at {}:{}",
-            self.action, self.read_or_write, self.file, self.line
-        )
-    }
-}
-
-#[derive(Debug, Copy, Clone, PartialEq, strum::Display)]
-#[strum(serialize_all = "snake_case")]
-enum ReadOrWrite {
-    Read,
-    Write,
-}
-
-#[derive(Debug, Copy, Clone, PartialEq, strum::Display)]
-enum Action {
-    #[strum(serialize = "waiting for")]
-    Waiting,
-    Acquired,
-}
-
-const BUFFER_SIZE: usize = 64;
-
-#[derive(Debug, Copy, Clone)]
-struct FixedSizeBuffer {
-    buffer: [Trace; BUFFER_SIZE],
-    index: usize,
-    size: usize,
-}
-
-impl FixedSizeBuffer {
-    fn add(&mut self, trace: Trace) {
-        self.buffer[self.index] = trace;
-        self.index = (self.index + 1) % BUFFER_SIZE;
-        self.size += 1;
-    }
-}
-
-impl Default for FixedSizeBuffer {
-    fn default() -> Self {
-        Self {
-            buffer: [Trace::default(); BUFFER_SIZE],
-            index: 0,
-            size: 0,
-        }
-    }
-}
-
-impl Index<usize> for FixedSizeBuffer {
-    type Output = Trace;
-
-    fn index(&self, index: usize) -> &Trace {
-        if index >= usize::min(self.size, BUFFER_SIZE) {
-            panic!("index is out of range");
-        }
-
-        let buffer_index = if self.size < BUFFER_SIZE {
-            index
-        } else {
-            (self.index + index) % BUFFER_SIZE
-        };
-        &self.buffer[buffer_index]
-    }
-}
-
-impl Display for FixedSizeBuffer {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.write_str("[\n")?;
-        for i in 0..usize::min(self.size, BUFFER_SIZE) {
-            write!(f, "\t{},\n", &self[i]);
-        }
-        f.write_str("]")
+        write!(f, "{}:{}", self.file, self.line)
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use std::panic;
 
     #[test]
-    fn test_fixed_size_buffer() {
-        {
-            let buffer = FixedSizeBuffer::default();
-            assert_eq!(format!("{}", buffer), "[  ]");
-        }
-        {
-            let mut buffer = FixedSizeBuffer::default();
-            buffer.add(Trace {
-                read_or_write: ReadOrWrite::Read,
-                action: Action::Waiting,
-                file: "file.rs",
-                line: 1,
-            });
-            assert_eq!(format!("{}", buffer), "[ waiting for read at file.rs:1 ]");
-        }
-        {
-            let mut buffer = FixedSizeBuffer::default();
-            for i in 0..BUFFER_SIZE + 1 {
-                buffer.add(Trace {
-                    read_or_write: ReadOrWrite::Read,
-                    action: Action::Waiting,
-                    file: "file.rs",
-                    line: i as u32,
-                });
-            }
-            assert_eq!(buffer[0].line, 1);
-            assert_eq!(buffer[BUFFER_SIZE - 1].line, 8);
+    fn test_recent_access() {
+        let mut recent_access = RecentAccess::Read(VecDeque::new());
+        assert_eq!(format!("{}", recent_access), "RwLock was last read: [\n]");
 
-            let prev_hook = panic::take_hook();
-            panic::set_hook(Box::new(|_| {}));
-            assert!(panic::catch_unwind(|| buffer[BUFFER_SIZE]).is_err());
-            panic::set_hook(Box::new(prev_hook));
+        recent_access.add_read(Trace {
+            file: "file.rs",
+            line: 1,
+        });
+        assert_eq!(
+            format!("{}", recent_access),
+            "RwLock was last read: [\n\tfile.rs:1,\n]"
+        );
 
-            let trace = Trace {
-                read_or_write: ReadOrWrite::Write,
-                action: Action::Waiting,
-                file: "file.rs",
-                line: 9,
-            };
-            buffer.add(trace);
-            assert_eq!(buffer[BUFFER_SIZE - 1], trace);
-        }
+        recent_access.add_write(Trace {
+            file: "file.rs",
+            line: 1,
+        });
+        assert_eq!(
+            format!("{}", recent_access),
+            "RwLock was last written: file.rs:1"
+        );
     }
 }
